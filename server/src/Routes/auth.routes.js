5de55dc19e9174b23127b44edcc8dd19174ebcd1
Jitesh <jiteshpal2604@gmail.com>
2025-10-02 02:58:25 +0530

    User routes created

--- server/src/Routes/auth.routes.js
+++ server/src/Routes/auth.routes.js
@@ -1,2 +1,10 @@
 import express from "express";
-import { signup, login, getMe } from ""
+import { signup, login, getMe } from "../Controllers/auth.controller";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/me", getMe);
+
+export default router;
