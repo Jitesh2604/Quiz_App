7ef8562078c3cb5ef2ed6572327328ba12a6a74c
Jitesh <jiteshpal2604@gmail.com>
2025-10-02 03:15:15 +0530

    result controller created

---
+++ server/src/Controllers/result.controller.js
@@ -0,0 +1,47 @@
+import Result from "../models/Result";
+
+export const saveResult = async (req, res) => {
+    try {
+        const { category, score, totalQuestions, correctAnswers } = req.body;
+
+        const userId = req.user.id;
+
+        const newResult = new Result({
+            user: userId,
+            category,
+            score,
+            totalQuestions,
+            correctAnswers,
+        });
+
+        await newResult.save();
+        res.status(201).json({ message: "Result saved successfully", result: newResult });
+    } catch (err) {
+        res.status(500).json({ message: "Error saving result", err });
+    }
+};
+
+export const getUserResults = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const results = await Result.find({ user: userId }).sort({ createdAt: -1 });
+        res.json(results);
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching results", err });
+    }
+};
+
+export const getLeaderboard = async (req, res) => {
+    try{
+        const { category } = req.params;
+        const leaderboard = await Result.find({ category})
+            .populate("user", "username email")
+            .sort({ score: -1 })
+            .limit(10);
+
+        res.json(leaderboard);
+    }catch(err){
+        res.status(500).json({ message: "Error fetching leaderboard", err });
+    }
+};
+
