70df5fa413f9397c6b079dbfccd07ad612e9df80
Jitesh <jiteshpal2604@gmail.com>
2025-10-02 02:21:59 +0530

    Result model created

---
+++ server/src/models/Result.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const resultSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        quizCategory: {
+            type: String,
+            required: true,
+        },
+        score: {
+            type: Number,
+            default: 0,
+        },
+        totalQuestions: {
+            type: Number,
+            required: true,
+        },
+        correctAnswers: {
+            type: Number,
+            required: true,
+        },
+    }, { timestamps: true }
+);
+
+const Result = mongoose.model("Result", resultSchema);
+
+export default Result;;
