7b67a3956cc8d9187f6d5e844b084c58e6cf6947
Jitesh <jiteshpal2604@gmail.com>
2025-10-03 00:42:56 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BackgroundEffects from "./components/BackgroundEffects";
-
-// Pages
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
@@ -10,23 +9,16 @@ import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-900 via-gray-800 to-black text-white">
-        {/* Navbar will always be visible */}
-        <BackgroundEffects />
-        <Navbar />
-
-        {/* Main content area */}
-        <main className="flex-1 container mx-auto px-4 py-6">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/quiz" element={<QuizPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-900 text-white">
+      <BackgroundEffects />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+      </Routes>
+    </div>
   );
 }
 
