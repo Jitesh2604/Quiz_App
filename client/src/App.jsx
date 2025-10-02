794b4210cb19f6a3c274bbb35066fd9fe52a05b4
Jitesh <jiteshpal2604@gmail.com>
2025-10-03 00:40:49 +0530

    main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import LoginPage from "./pages/Login";
 import BackgroundEffects from "./components/BackgroundEffects";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white font-sans relative overflow-hidden">
-      <BackgroundEffects />
-      <Navbar />
-      <main className="relative z-10">
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <LoginPage />
-        </div>
-      </main>
-    </div>
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-900 via-gray-800 to-black text-white">
+        {/* Navbar will always be visible */}
+        <BackgroundEffects />
+        <Navbar />
+
+        {/* Main content area */}
+        <main className="flex-1 container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/quiz" element={<QuizPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
