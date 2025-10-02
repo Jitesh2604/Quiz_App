38d8ab02c13549ad37cdb327e34b0cfcaeb2d20a
Jitesh <jiteshpal2604@gmail.com>
2025-10-03 00:24:47 +0530

    BackgroundEffect

---
+++ client/src/components/BackgroundEffects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default  const BackgroundEffects = () => {
+    return (
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden -z-10">
+            <div className="absolute top-[10%] left-[5%] w-48 h-48 bg-cyan-500/20 rounded-full animate-pulse blur-2xl"></div>
+            <div className="absolute top-[20%] right-[10%] w-64 h-64 bg-blue-500/20 rounded-full animate-pulse delay-1000 blur-3xl"></div>
+            <div className="absolute bottom-[15%] left-[20%] w-40 h-40 bg-purple-500/20 rounded-full animate-pulse delay-500 blur-2xl"></div>
+            <div className="absolute bottom-[5%] right-[25%] w-52 h-52 bg-teal-500/20 rounded-full animate-pulse delay-2000 blur-3xl"></div>
+        </div>
+    );
+};
