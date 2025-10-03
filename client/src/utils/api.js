cfba560e5353380e3a2f42f9c7136baf29084054
Jitesh <jiteshpal2604@gmail.com>
2025-10-03 06:27:05 +0530

    update Api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -15,4 +15,43 @@ API.interceptors.request.use((config) => {
     return config;
 });
 
+// ============================
+// Categories
+// ============================
+export const getCategories = async () => {
+    const res = await API.get("/categories");
+    return res.data;
+  };
+  
+  // ============================
+  // Questions
+  // ============================
+  export const getQuestions = async ({ category, limit }) => {
+    const res = await API.post("/questions", { category, limit });
+    return res.data;
+  };
+  
+  // ============================
+  // Quiz Results
+  // ============================
+  export const getUserResults = async (userId) => {
+    const res = await API.get(`/quiz/results/${userId}`);
+    return res.data;
+  };
+  
+  export const submitQuiz = async (data) => {
+    const res = await API.post("/quiz/submit", data);
+    return res.data;
+  };
+  
+  // ============================
+  // Leaderboard
+  // ============================
+  export const getLeaderboard = async () => {
+    const res = await API.get("/leaderboard");
+    return res.data;
+  };
+  
+  
+
 export default API;
