d06b23313cbe59d984c05ae1dd5a5cc33d706f01
Jitesh <jiteshpal2604@gmail.com>
2025-10-04 11:59:12 +0530

    User Conter

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect} from "react";
 // The fix is on the next line:
-import { jwtDecode } from "jwt-decode"; 
+import jwtDecode from "jwt-decode"; 
 
 const UserContext = createContext();
 
